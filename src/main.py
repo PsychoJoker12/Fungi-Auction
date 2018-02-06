2534d663737295ec094734445bdaedf5b4428e55
Robert Ciliberto <rciliberto2@bostonk12.org>
2018-02-06 14:13:56 -0500

    Add test fungus

--- src/main.py
+++ src/main.py
@@ -5,6 +5,10 @@ from fungus import Fungus
 
 def main():
     """main method"""
+    fungus1_adaptations = {0,1}
+
+    fungus1 = Fungus(fungus1_adaptations)
+    print(fungus1.adaptations)
 
 if __name__ == "__main__":
     main()
