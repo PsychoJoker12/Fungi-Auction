193fac2500a8ed6952451d1562f30b231fd1cf0c
Robert Ciliberto <rciliberto2@bostonk12.org>
2018-03-23 13:51:21 -0400

    add check_environment method which returns the correct multiplierdepending on current environment

--- src/adaptation.py
+++ src/adaptation.py
@@ -1,10 +1,19 @@
+import reference
+
 class Adaptation(object):
     """An adaptation which allows a fungus to accomplish some task"""
 
-    def __init__(self, name="Default Adaptation", multiplier=1):
+    def __init__(self, name="Default Adaptation", multiplier=1, suitable_environments=[]):
         """Specify a name and an optional constant multiplier"""
         self.name = name
         self.multiplier = multiplier
+        self.suitable_environments = suitable_environments
     
     def __str__(self):
         return self.name
+
+    def check_environment(self, environment=reference.environments[9]):
+        if environment in self.suitable_environments:
+            return self.multiplier
+        else:
+            return 1
